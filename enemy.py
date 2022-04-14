abb4297a2f07640f05b346de84ba185f4a9bbbab
Brady_Brandt1 <bradybrandt3@gmail.com>
2022-04-14 09:46:17 -0500

    Removed files

--- enemy.py
+++ enemy.py
@@ -1,53 +1,81 @@
 import pygame
 
 class Enemy:
-	# takes in starting location and array of 3d tuples
-	# tuple specifies the x direction to move and the y
-	# final final indicates how many times to move in that direction
-	def __init__(self, screen, location, movement):
-		self.screen = screen
-		
-		self.x = location[0]
-		self.y = location[1]
-		self.radius = 12
-		self.speed = 75
-		self.color = (0,0,255)
-		self.center = (self.x + self.radius, self.y + self.radius)
-		self.movement = movement
-		
-		# parse our movement tuple into an array of 3d vectors
-		movement = []
-		current = []
-		for move in self.movement:
-			current.append(move)
-			if len(current) == 3:
-				movement.append(current)
-				current = []
-		self.movement = movement
+    # takes in starting location and array of 3d tuples
+    # tuple specifies the x direction to move and the y
+    # final final indicates how many times to move in that direction
+    def __init__(self, screen, location, movement):
+        self.screen = screen
+        
+        self.x = location[0]
+        self.y = location[1]
+        self.radius = 12
+        self.speed = 75
+        self.color = (0,0,255)
+        self.center = (self.x + self.radius, self.y + self.radius)
+        self.movement = movement
+        
+        # parse our movement tuple into an array of 3d vectors
+        movement = []
+        current = []
+        for move in self.movement:
+            current.append(move)
+            if len(current) == 3:
+                movement.append(current)
+                current = []
+        self.movement = movement
 
 
-		# index into movement array
-		self.index = 0
+        # index into movement array
+        self.index = 0
 
-		self.startx = self.x
-		self.starty = self.y	
+        self.startx = self.x
+        self.starty = self.y
 
-	def get_circle(self, rect):
+    # returns the distance squared between two points
+    def distance_squared(self, point1, point2):
+        x1,y1 = point1
+        x2,y2 = point2
 
-	def draw(self, dt):
-		# keeps track of how far to move in a direction
-		diffx = abs(self.x - self.startx)
-		diffy = abs(self.y - self.starty)
-		amount_to_move = self.movement[self.index][2]
+        return (y2-y1) ** 2 + (x2 - x1) ** 2
 
-		if diffx >= amount_to_move or diffy >= amount_to_move:
-			self.index += 1
-			if self.index == len(self.movement):
-				self.index = 0
-			self.startx = self.x
-			self.starty = self.y
+    # takes in the player rect and returns a boolean if they collide 
+    def collide_player(self, player):
+        self.center = (self.x + self.radius, self.y + self.radius)
+        radius_sq = self.radius ** 2
 
-		self.x += self.speed * self.movement[self.index][0] * dt
-		self.y += self.speed * self.movement[self.index][1] * dt
-		pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
-		
+
+        if self.distance_squared(player.center, self.center) == radius_sq - (player.left - player.right) / 2:
+            return True
+        # check if the circle collides with any four corners of the player
+        if self.distance_squared(player.topleft, self.center) == radius_sq:
+            return True
+
+        if self.distance_squared(player.topright, self.center) == radius_sq:
+            return True
+
+        if self.distance_squared(player.bottomleft, self.center) == radius_sq:
+            return True
+
+        if self.distance_squared(player.bottomleft, self.center) == radius_sq:
+            return True
+    
+        return False
+
+    def draw(self, dt):
+        # keeps track of how far to move in a direction
+        diffx = abs(self.x - self.startx)
+        diffy = abs(self.y - self.starty)
+        amount_to_move = self.movement[self.index][2]
+
+        if diffx >= amount_to_move or diffy >= amount_to_move:
+            self.index += 1
+            if self.index == len(self.movement):
+                self.index = 0
+            self.startx = self.x
+            self.starty = self.y
+
+        self.x += self.speed * self.movement[self.index][0] * dt
+        self.y += self.speed * self.movement[self.index][1] * dt
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+        
--- game.py
+++ game.py
@@ -4,107 +4,109 @@ from enemy import Enemy
 from objects import Rectangle, Border, Checkpoint
 
 class Game:
-	def __init__(self, screen, dims, player):
-		self.screen = screen 
-		self.width = dims[0]
-		self.height = dims[1]
-		self.player = player
-		self.level = 1
-		self.deaths = 0
-		self.newLevel = True
-		self.currentLevel = ""
+    def __init__(self, screen, dims, player):
+        self.screen = screen 
+        self.width = dims[0]
+        self.height = dims[1]
+        self.player = player
+        self.level = 1
+        self.deaths = 0
+        self.newLevel = True
+        self.currentLevel = ""
 
-		pygame.font.init()
-		self.gameFont = pygame.font.SysFont("Arial", 45)
-	
+        pygame.font.init()
+        self.gameFont = pygame.font.SysFont("Arial", 45)
 
-	def draw_hud(self):
-		 # draws black rectangle at top of screen
-		 pygame.draw.rect(self.screen, (0,0,0), (0,0,self.width, 50))
-		 # draws black rectangel at bottom of screen
-		 pygame.draw.rect(self.screen, (0,0,0), (0,self.height-50,self.width,50))
 
-		 # draws the level text
-		 level_text = self.gameFont.render("Level: " + str(self.level),False, (255,255,255))		
-		 self.screen.blit(level_text, (5,0))
+    def draw_hud(self):
+        # draws black rectangle at top of screen
+         pygame.draw.rect(self.screen, (0,0,0), (0,0,self.width, 50))
+         # draws black rectangel at bottom of screen
+         pygame.draw.rect(self.screen, (0,0,0), (0,self.height-50,self.width,50))
 
-		 # draws the fails text
-		 fail_text = self.gameFont.render("Fails: " + str(self.deaths), False, (255,255,255))
-		 self.screen.blit(fail_text, (self.width - 200, 0))
+         # draws the level text
+         level_text = self.gameFont.render("Level: " + str(self.level),False, (255,255,255))		
+         self.screen.blit(level_text, (5,0))
 
-	def play_game(self, dt):
-		if self.newLevel:
-			self.currentLevel = Level(self.screen, self.level)
-			self.currentLevel.load_level()
-			spawn = self.currentLevel.checkpoints[0].get_spawn_loc()
-			self.player.set_spawn_point(spawn[0], spawn[1])
-			self.player.spawn()
-			self.newLevel = False
-	
-		self.draw_hud()
-		self.currentLevel.draw_level(self.player, dt)
-		self.player.move(dt,self.currentLevel.get_borders())
-		
+         # draws the fails text
+         fail_text = self.gameFont.render("Fails: " + str(self.deaths), False, (255,255,255))
+         self.screen.blit(fail_text, (self.width - 200, 0))
+
+    def play_game(self, dt):
+        if self.newLevel:
+            self.currentLevel = Level(self.screen, self.level)
+            self.currentLevel.load_level()
+            spawn = self.currentLevel.checkpoints[0].get_spawn_loc()
+            self.player.set_spawn_point(spawn[0], spawn[1])
+            self.player.spawn()
+            self.newLevel = False
+
+        self.draw_hud()
+        self.currentLevel.draw_level(self.player, dt)
+        self.player.move(dt,self.currentLevel.get_borders())
+
 
 class Level:
-	def __init__(self, screen, level):
-		self.screen = screen
-		self.hasLoaded = False		
-		self.attributes = ["CHECKPOINT", "ENEMY", "BORDER", "RECT"]
-		#these lists hold all of the objects in the current level
-		self.checkpoints = []
-		self.enemies = []
-		self.borders = []
-		self.rectangles = []
-		self.level = level
-		self.hasCollided = False
+    def __init__(self, screen, level):
+        self.screen = screen
+        self.hasLoaded = False		
+        self.attributes = ["CHECKPOINT", "ENEMY", "BORDER", "RECT"]
+        #these lists hold all of the objects in the current level
+        self.checkpoints = []
+        self.enemies = []
+        self.borders = []
+        self.rectangles = []
+        self.level = level
+        self.hasCollided = False
 
-	def parse_data(self, attributes):	
-		#create all our object and add them to level arrays
-		for obj in attributes:
-			if len(obj) < 1:
-				continue
-			if obj[0] == "CHECKPOINT":
-				checkpoint = Checkpoint(self.screen, obj[1])
-				self.checkpoints.append(checkpoint)
-			elif obj[0] == "BORDER":
-				border= Border(self.screen, obj[1])
-				self.borders.append(border)
-			elif obj[0] == "RECT":
-				rect = Rectangle(self.screen, obj[1])
-				self.rectangles.append(rect)
-			elif obj[0] == "ENEMY":
-				enemy = Enemy(self.screen, obj[1], obj[2])
-				self.enemies.append(enemy)
-					 
-					
-	def load_level(self):
-		f = open("levels/level" + str(self.level) + ".txt", "r")
-		data = f.read()
-		
-		blocks = data.split("END\n\n")
-		for block in blocks:
-			attributes = parser.parse_block(block)
-			self.parse_data(attributes)
-		self.hasLoaded = True
-		f.close()
+    def parse_data(self, attributes):	
+        #create all our object and add them to level arrays
+        for obj in attributes:
+            if len(obj) < 1:
+                continue
+            if obj[0] == "CHECKPOINT":
+                checkpoint = Checkpoint(self.screen, obj[1])
+                self.checkpoints.append(checkpoint)
+            elif obj[0] == "BORDER":
+                border= Border(self.screen, obj[1])
+                self.borders.append(border)
+            elif obj[0] == "RECT":
+                rect = Rectangle(self.screen, obj[1])
+                self.rectangles.append(rect)
+            elif obj[0] == "ENEMY":
+                enemy = Enemy(self.screen, obj[1], obj[2])
+                self.enemies.append(enemy)
 
-	def draw_level(self, player, dt):
-		player_rect = player.get_rect()
-		for rectangle in self.rectangles:
-			rectangle.draw()
-		for border in self.borders:
-			border.draw()	
-		for checkpoint in self.checkpoints:
-			checkpoint.draw()
-			# sets the players spawn to the checkpoint
-			if checkpoint.get_rect().colliderect(player_rect):
-				spawn = checkpoint.get_spawn_loc()
-				player.set_spawn_point(spawn[0], spawn[1])
-	
 
-		for enemy in self.enemies:
-			enemy.draw(dt)
+    def load_level(self):
+        f = open("levels/level" + str(self.level) + ".txt", "r")
+        data = f.read()
 
-	def get_borders(self):
-		return self.borders
+        blocks = data.split("END\n\n")
+        for block in blocks:
+            attributes = parser.parse_block(block)
+            self.parse_data(attributes)
+        self.hasLoaded = True
+        f.close()
+
+    def draw_level(self, player, dt):
+        player_rect = player.get_rect()
+        for rectangle in self.rectangles:
+            rectangle.draw()
+        for border in self.borders:
+            border.draw()	
+        for checkpoint in self.checkpoints:
+            checkpoint.draw()
+            # sets the players spawn to the checkpoint
+            if checkpoint.get_rect().colliderect(player_rect):
+                spawn = checkpoint.get_spawn_loc()
+                player.set_spawn_point(spawn[0], spawn[1])
+
+
+        for enemy in self.enemies:
+            enemy.draw(dt)
+            if enemy.collide_player(player_rect):
+                print("Collide")
+
+    def get_borders(self):
+        return self.borders
