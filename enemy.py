5f568378d1e2e78766c42bd43ea3113cdc41a1d0
Brady_Brandt1 <bradybrandt3@gmail.com>
2022-04-17 10:01:05 -0500

    Fixed issue with bots going offscreen

--- enemy.py
+++ enemy.py
@@ -10,7 +10,7 @@ class Enemy:
         self.x = location[0]
         self.y = location[1]
         self.radius = 10
-        self.speed = 75
+        self.speed = 90
         self.color = (0,0,255)
         self.movement = movement
         
@@ -30,32 +30,45 @@ class Enemy:
 
         self.startx = self.x
         self.starty = self.y
-        self.firstFrame = True
+
     def get_center(self):
         return (self.x, self.y)
 
     def get_radius(self):
         return self.radius
 
+    # returns the normalized sign of a value
+    def get_dirrection(self, value):
+        if value < 0:
+            return value / value * -1
+        return value / value
+
     def draw(self, dt):
-        # the dt from the first frame causes the 
-        # enemy to be teleported to the wrong spot 
-        # because the delta time is so large
-        # so we just skip the first frame instead
-        if self.firstFrame:
-            self.firstFrame = False
-            return
 
         # keeps track of how far to move in a direction
         diffx = abs(self.x - self.startx)
         diffy = abs(self.y - self.starty)
+
+        # the amount to move in one direction
         amount_to_move = self.movement[self.index][2]
 
-        if diffx >= amount_to_move or diffy >= amount_to_move:
+        if diffx >= amount_to_move:
             self.index += 1
             if self.index == len(self.movement):
                 self.index = 0
+
+            # make sure the bot doesn't end up to far of path if dt is too large
+            dirrection = self.get_dirrection(self.movement[self.index][0])  
+            self.x = self.startx + dirrection * amount_to_move * -1 
             self.startx = self.x
+
+        if diffy >= amount_to_move:
+            self.index += 1
+            if self.index == len(self.movement):
+                self.index = 0
+
+            dirrection = self.get_dirrection(self.movement[self.index][1])
+            self.y = self.starty + dirrection * amount_to_move * -1
             self.starty = self.y
 
         self.x += self.speed * self.movement[self.index][0] * dt
