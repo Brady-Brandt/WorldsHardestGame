34b5bf936366ef7c5c6a3f3a7602253026c1eecd
Brady-Brandt <bradybrandt3@gmail.com>
2024-08-05 16:07:28 -0500

    remove vsync and added __main__ if statement

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import pygame
 from player import Player
-from enemy import Enemy
 from game import Game
 
 SCREEN_DIM = (800, 800)
 WHITE = (255, 255, 255)
 BACKGROUND = (100, 149, 237)
-pygame.init()
 
-screen = pygame.display.set_mode(SCREEN_DIM, vsync=1)
-pygame.display.set_caption("World's Hardest Game")
 
-player = Player(screen, SCREEN_DIM)
-game = Game(screen, SCREEN_DIM, player) 
-quit = False
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_DIM)
+    pygame.display.set_caption("World's Hardest Game")
 
-last_frame = 0
+    player = Player(screen, SCREEN_DIM)
+    game = Game(screen, SCREEN_DIM, player) 
+    quit = False
 
-while not quit:
-	time = pygame.time.get_ticks()
-	delta_time = (time - last_frame) / 1000.0
-	last_frame = time
+    last_frame = 0
 
-	screen.fill(BACKGROUND)	
-	game.play_game(delta_time)
+    while not quit:
+        time = pygame.time.get_ticks()
+        delta_time = (time - last_frame) / 1000.0
+        last_frame = time
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			quit = True
+        screen.fill(BACKGROUND)	
+        game.play_game(delta_time)
 
-	pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit = True
+
+        pygame.display.flip()
