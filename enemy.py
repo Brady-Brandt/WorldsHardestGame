64a4b349a5b568166402722a0b7e503a3d58eb05
Brady-Brandt <bradybrandt3@gmail.com>
2024-08-09 11:12:22 -0500

    Added an methods for circular motion

--- enemy.py
+++ enemy.py
@@ -1,23 +1,32 @@
 import pygame
+import math
+
 
 class Enemy:
-    color = (0,0,255)
+    color = (0, 0, 255)
     radius = 10
     # takes in starting location and list of 3d tuples
     # tuple specifies the x direction to move and the y
-    # last one defines how far to move in that direction 
+    # last one defines how far to move in that direction
+
     def __init__(self, location, movement, speed=215, sync=True):
         self.x = location[0]
         self.y = location[1]
         self.speed = speed
-        self.movement = movement      
+        self.movement = movement
         # index into movement array
         self.index = 0
-        self.total_moved = 0 
+        self.total_moved = 0
 
         self.sync = sync
         self.startx = self.x
         self.starty = self.y
+
+        self.circular = False
+        self.theta = 0
+        self.radius = 0
+        self.circle_center = (0, 0)
+        self.dir = 1
 
     def get_center(self):
         return (self.x, self.y)
@@ -26,7 +35,7 @@ class Enemy:
         return Enemy.radius
 
     # returns the normalized sign of a value
-    def get_dirrection(self,value):
+    def get_dirrection(self, value):
         if value > 0:
             return 1
         elif value < 0:
@@ -42,14 +51,13 @@ class Enemy:
                 self.y = self.starty
             self.index = 0
 
-    # so we don't lose movement when the enemy switches direction 
-    def overshoot(self, val): 
+    # so we don't lose movement when the enemy switches direction
+    def overshoot(self, val):
         self.update_movement()
         amount_to_move = self.movement[self.index][2]
         if amount_to_move == 0:
             self.update_movement()
             amount_to_move = self.movement[self.index][2]
-
 
         x = self.movement[self.index][0]
         y = self.movement[self.index][1]
@@ -57,15 +65,23 @@ class Enemy:
         self.x += self.get_dirrection(x) * val
         self.y += self.get_dirrection(y) * val
         self.total_moved += val
-        
-
 
     def draw(self, screen, dt):
+        if self.circular:
+            self.theta += (self.speed * dt) * self.dir
+            self.theta %= 2 * math.pi
+            self.x = self.circle_center[0] + self.radius * math.cos(self.theta)
+            self.y = self.circle_center[1] + self.radius * math.sin(self.theta)
+            pygame.draw.circle(
+                screen, Enemy.color, (self.x, self.y), Enemy.radius)
+            return
+
         # the amount to move in one direction
         amount_to_move = self.movement[self.index][2]
         if amount_to_move == 0:
             self.update_movement()
-            pygame.draw.circle(screen, Enemy.color, (self.x, self.y), Enemy.radius)
+            pygame.draw.circle(
+                screen, Enemy.color, (self.x, self.y), Enemy.radius)
             return
 
         x = self.movement[self.index][0]
@@ -89,6 +105,14 @@ class Enemy:
                 overshoot = self.total_moved - amount_to_move
                 self.y -= overshoot * self.get_dirrection(y)
                 self.overshoot(overshoot)
-            
-            
+
         pygame.draw.circle(screen, Enemy.color, (self.x, self.y), Enemy.radius)
+
+    # speed is in degrees
+    def add_circular_motion(self,circle_center,radius,speed=20,direction=1):
+        self.radius = radius
+        self.circular = True
+        self.circle_center = circle_center
+        self.theta = math.atan2(self.starty - circle_center[1],self.startx - circle_center[0])
+        self.speed = math.radians(speed)
+        self.dir = direction
